09ab3e50393bc6b87eecc973fb357e28fc21285a
Anastasya Tikhomirova <cerera.qa@gmail.com>
2024-09-10 08:36:03 +0300

    5.4 - done

--- Lesson_5/5.4.py
+++ Lesson_5/5.4.py
@@ -12,23 +12,24 @@ try:
     wait = WebDriverWait(chrome, 10)
     modal_window = wait.until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, ".modal")))
-    close_button = wait.until(EC.element_to_be_clicable(
-        (By.CSS_SELECTOR, "modal-footer")))
+    close_button = wait.until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, ".modal-footer")))
     time.sleep(3)
     close_button.click()
-        time.sleep(2)
+    time.sleep(2)
     
     firefox.get("http://the-internet.herokuapp.com/entry_ad")
     wait = WebDriverWait(firefox, 10)
     modal_window = wait.until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, ".modal")))
-    close_button = wait.until(EC.element_to_be_clicable(
-        (By.CSS_SELECTOR, "modal-footer")))
+    close_button = wait.until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, ".modal-footer")))
     time.sleep(3)
     close_button.click()
-        time.sleep(2)
-
+    time.sleep(2)
+    
 except Exception as ex:
     print(ex)
 finally:
-    driver.quit()
+    chrome.quit()
+    firefox.quit()
