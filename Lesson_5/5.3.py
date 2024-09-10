128cb4043d49ba3547e8c9dd8dbca50dd1c133fe
Anastasya Tikhomirova <cerera.qa@gmail.com>
2024-09-10 08:28:51 +0300

    fix

--- Lesson_5/5.3.py
+++ Lesson_5/5.3.py
@@ -1,22 +1,19 @@
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from time import sleep
 
 chrome = webdriver.Chrome()
 firefox = webdriver.Firefox()
-
 try:
     chrome.get("http://uitestingplayground.com/classattr")
     for _ in range(3):
-        blue_button = chrome.find_element(
-            "xpath", "//button[contains(concat(' ', normalize-space(@class), ' ') ' btn-primary ')]")
+        blue_button = chrome.find_element("xpath", "//button[contains(concat(' ', normalize-space(@class), ' '), 'btn-primary')]")
         blue_button.click()
         sleep(2)
         chrome.switch_to.alert.accept()
     firefox.get("http://uitestingplayground.com/classattr")
     for _ in range(3):
-        blue_button = firefox.find_element(
-            "xpath", "//button[contains(concat(' ', normalize-space(@class), ' ') ' btn-primary ')]")
+        blue_button = firefox.find_element("xpath", "//button[contains(concat(' ', normalize-space(@class), ' '), 'btn-primary')]")
         blue_button.click()
         sleep(2)
         firefox.switch_to.alert.accept()
@@ -24,4 +21,5 @@ try:
 except Exception as ex:
     print(ex)
 finally:
-    driver.quit()
+    chrome.quit()
+    firefox.quit()
