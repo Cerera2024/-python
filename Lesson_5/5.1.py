f5840b37cc17550b4b94ca61f7fa25407e88f5ef
Anastasya Tikhomirova <cerera.qa@gmail.com>
2024-09-10 07:00:00 +0300

    add homework5

--- Lesson_5/5.1.py
+++ Lesson_5/5.1.py
@@ -14,18 +14,20 @@ try:
         By.XPATH, '//button[text()="Add Element"]').click()
     add_button = firefox.find_element(
         By.XPATH, '//button[text()="Add Element"]').click()
-        sleep(1)
-        chrome_delete_buttons = chrome.find_elements(
-        "xpath", '//button[text()="Delete"]').click()
-        firefox_delete_buttons = firefox.find_elements(
-        "xpath", '//button[text()="Delete"]').click()
+    sleep(1)
+    
+    chrome_delete_buttons = chrome.find_elements(
+        "xpath", '//button[text()="Delete"]')
+    firefox_delete_buttons = firefox.find_elements(
+        "xpath", '//button[text()="Delete"]')
 
-print(
-    f"Размер списка кнопок Delete в Chrome: {len(chrome_delete_buttons)}")
-print(
-    f"Размер списка кнопок Delete в Firefox: {len(firefox_delete_buttons)}")
+    print(
+            f"Размер списка кнопок Delete в Chrome: {len(chrome_delete_buttons)}")
+    print(
+            f"Размер списка кнопок Delete в Firefox: {len(firefox_delete_buttons)}")
 
 except Exception as ex:
     print(ex)
 finally:
-    driver.quit()
+    chrome.quit() # type: ignore
+    firefox.quit()
