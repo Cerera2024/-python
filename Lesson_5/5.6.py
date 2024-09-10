cfac65f0fa6d012c8abb3f2f35d1ae61b40a6636
Anastasya Tikhomirova <cerera.qa@gmail.com>
2024-09-10 08:42:21 +0300

    5.6-done

--- Lesson_5/5.6.py
+++ Lesson_5/5.6.py
@@ -1,28 +1,29 @@
 from selenium import webdriver
-from time import sleep
 from selenium.webdriver.common.by import By
+from time import sleep
 
 chrome = webdriver.Chrome()
 firefox = webdriver.Firefox()
 
 try:
     chrome.get("http://the-internet.herokuapp.com/login")
-    input_name = chrome.find_element(By.ID, "username").send_keys("tomsmith")
+    input_name = chrome.find_element(By.ID, "username")
+    input_name.send_keys("tomsmith")
     sleep(1)
-    input_pass = chrome.find_element(
-        By.ID, "password").send_keys("SuperSecretPassword!")
+    input_pass = chrome.find_element(By.ID, "password")
+    input_pass.send_keys("SuperSecretPassword!")
     sleep(2)
 
     firefox.get("http://the-internet.herokuapp.com/login")
-    input_name = firefox.find_element(By.ID, "username").send_keys("tomsmith")
+    input_name = firefox.find_element(By.ID, "username")
+    input_name.send_keys("tomsmith")
     sleep(1)
-    input_pass = firefox.find_element(
-        By.ID, "password").send_keys("SuperSecretPassword!")
+    input_pass = firefox.find_element(By.ID, "password")
+    input_pass.send_keys("SuperSecretPassword!")
     sleep(2)
-
-
 
 except Exception as ex:
     print(ex)
 finally:
-    driver.quit()
+    chrome.quit()
+    firefox.quit()
